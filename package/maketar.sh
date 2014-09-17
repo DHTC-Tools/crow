e631003884df7febef02a2635df96a5fccf0658d
David Champion <dgc@uchicago.edu>
2014-09-17 17:09:00 -0500

    BUILD -> maketar.sh

---
+++ package/maketar.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+version=${1-HEAD}
+tarball=crow.$(date +%Y%m%d)git${version}.tar.gz
+dir=$(dirname "$0")
+cd $dir/..
+git archive --format tar --prefix=crow/ ${version} . |
+gzip -9 > $tarball
