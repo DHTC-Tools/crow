e9d9521714ebfca0835a186aca5a9d3b9be68c36
David Champion <dgc@uchicago.edu>
2014-09-17 17:50:41 -0500

    how I update my pkg

---
+++ package/update.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+date=$(date +%Y%m%d)
+set -x
+
+# uninstall old pkg
+rpm -e crow
+
+# make new pkg
+./package/maketar.sh HEAD || exit 1
+mv crow.${date}gitHEAD.tar.gz ~/rpmbuild/SOURCES/ || exit 1
+rpmbuild -ba package/crow.spec || exit 1
+
+# install new pkg
+rpm -ip ~/rpmbuild/RPMS/noarch/crow-${date}gitHEAD-1.el6.noarch.rpm
+
+mv /etc/sysconfig/crow.rpmsave /etc/sysconfig/crow || exit 1
+mv /etc/crow.ini.rpmsave /etc/crow.ini || exit 1
